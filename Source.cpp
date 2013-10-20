ee75f52b8bedc9f34918e1cd3dfffefe6a3dab0f
Parthas-Menethil <xywhpanda@gmail.com>
2013-10-20 15:32:51 -0400

    Implement Merge Sort #2

--- Source.cpp
+++ Source.cpp
@@ -5,33 +5,47 @@
 #include <cstdlib>
 #include <algorithm>
 using namespace std;
-void TopDownMergeSort(Package A[], Package B[], int n);
-void TopDownSplitMerge(Package A[], int iBegin, int iEnd, Package B[]);
-void TopDownMerge(Package A[], int iBegin, int iMiddle, int iEnd, Package B[]);
-void TopDownMergeSort(Package A[], Package B[], int n){
-	TopDownSplitMerge(A, 0, n, B);
+void merge(Package*, Package*, int, int, int);
+void mergeSort(Package *a, Package *b, int low, int high){
+	int pivot;
+	if (low < high){
+		pivot = (low + high) / 2;
+		mergeSort(a, b, low, pivot);
+		mergeSort(a, b, pivot + 1, high);
+		merge(a, b, low, pivot, high);
+		
+	}
 }
-void TopDownSplitMerge(Package A[], int iBegin, int iEnd, Package B[]){
-	if (iEnd - iBegin < 2)
-		return;
-	int iMiddle = 0;
-	iMiddle = (iBegin + iEnd) / 2;
-	TopDownSplitMerge(A, iBegin, iMiddle, B);
-	TopDownSplitMerge(A, iBegin, iEnd, B);
-	TopDownMerge(B, iBegin, iMiddle, iEnd, A);
-	copy(B, B + (iEnd - iBegin), A);
-}
-void TopDownMerge(Package A[], int iBegin, int iMiddle, int iEnd, Package B[]){
-	int i0 = iBegin, i1 = iMiddle;
-	
-	for (int j = iBegin; j < iEnd; j++){
-		if (i0 < iMiddle && (i1 >= iEnd || A[i0].weight <= A[i1].weight)){
-			B[j] = A[i0++];
+void merge(Package *a, Package *b, int low, int pivot, int high){
+	int h, i, j, k;
+	h = low;
+	i = low;
+	j = pivot + 1;
+	while((h <= pivot) && (j <= high)){
+		if (a[h].weight <= a[j].weight){
+			b[i] = a[h];
+			h++;
 		}
 		else{
-			B[j] = A[i1++];
+			b[i] = a[j];
+			j++;
+		}
+		i++;
+	}
+	if (h > pivot){
+		for (k = j; k <= high; k++){
+			b[i] = a[k];
+			i++;
 		}
 	}
+	else{
+		for (k = h; k <= pivot; k++){
+			b[i] = a[k];
+			i++;
+		}
+	}
+	for (k = low; k <= high; k++)
+		a[k] = b[k];
 }
 
 int main(){
@@ -46,9 +60,9 @@ int main(){
 		pkgs.push_back(Package(maxFloor));
 		cout << "Weight:" << pkgs[i].weight << "kg To:" << pkgs[i].destFloor << endl;
 	}
-	Package p[10];
-	TopDownMergeSort(pkgs.data(), p, 9);
-	for (int i = 0; i < 10; i++)
+	Package p[packNumber];
+	mergeSort(pkgs.data(), p, 0, packNumber - 1);
+	for (int i = 0; i < packNumber; i++)
 		cout << "Weight: " << p[i].weight << "kg To:" << p[i].destFloor << endl;
 	return 0;
 }
