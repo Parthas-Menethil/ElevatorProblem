5519fa629b13231404010ff6c614565938d50c34
Parthas-Menethil <xywhpanda@gmail.com>
2013-10-20 16:45:44 -0400

    Enhance output system. Fix bug while there are less than 2 packages.

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,16 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
-#include <algorithm>
+
+#define __DESC(show, msg) {if (show == true) cout << msg << endl;}; 
+#define __SWITCH_OUTPUT__ true
 using namespace std;
+/* Merge Sort Function
+ * @Source: http://simplestcodings.blogspot.ca/2010/08/merge-sort-implementation-in-c.html
+ * @Author: Varun Gupta
+ * @Date: 2010-08-03
+ * @Modified by: Luke
+ * */
 void merge(Package*, Package*, int, int, int);
 void mergeSort(Package *a, Package *b, int low, int high){
 	int pivot;
@@ -13,7 +21,10 @@ void mergeSort(Package *a, Package *b, int low, int high){
 		mergeSort(a, b, low, pivot);
 		mergeSort(a, b, pivot + 1, high);
 		merge(a, b, low, pivot, high);
-		
+	}
+	else{
+		if (b && a)
+			b[0] = a[0];
 	}
 }
 void merge(Package *a, Package *b, int low, int pivot, int high){
@@ -48,24 +59,16 @@ void merge(Package *a, Package *b, int low, int pivot, int high){
 		a[k] = b[k];
 }
 // Greedy Loading
-float loading(Package *pkgs, Package *p, int num){
-	mergeSort(pkgs, p, 0, num - 1);
-	float tWeight = 0, loadCapacity = 10;
-	for (int i = 0; i < num && p[i].weight <= loadCapacity; i++){
-		p[i].loaded = true;
-		tWeight += p[i].weight;
-		loadCapacity -= p[i].weight;
-	}
-	return tWeight;
-}
 int greedyLoading(Package *pkgs, const int num){
 	int totTime = 0;
 	Package p[num];
 	mergeSort(pkgs, p, 0, num - 1);
 	int processedPackages = 0;
 	while (processedPackages < num){
+		__DESC (__SWITCH_OUTPUT__, "Elevator start loading...");
 		float tWeight = 0, loadCapacity = 10, maxFloor = 0;
 		for (; processedPackages < num && p[processedPackages].weight <= loadCapacity; processedPackages++){
+			__DESC (__SWITCH_OUTPUT__, "->Load " << p[processedPackages].weight << " kgs");
 			p[processedPackages].loaded = true;
 			if (p[processedPackages].destFloor > maxFloor)
 				maxFloor = p[processedPackages].destFloor;
@@ -73,6 +76,8 @@ int greedyLoading(Package *pkgs, const int num){
 			loadCapacity -= p[processedPackages].weight;
 		}
 		totTime += maxFloor * 2;
+		__DESC (__SWITCH_OUTPUT__, "->To floor " << maxFloor);
+		__DESC (__SWITCH_OUTPUT__, "->Time span: " << maxFloor * 2);
 	}
 	return totTime;
 }
@@ -84,12 +89,13 @@ int main(){
 	cin >> maxFloor;
 	cout << "Please enter the package number: ";
 	cin >> packNumber;
+	cout <<"====== Generating Packages ======" << endl;
 	for(int i = 0; i < packNumber; i++){
 		pkgs.push_back(Package(maxFloor));
 		cout << "Weight:" << pkgs[i].weight << "kg To:" << pkgs[i].destFloor << endl;
 	}
-	cout << "Start Loading..." << endl;
-	cout << "Greedy Elevator:" << greedyLoading(pkgs.data(), packNumber) << endl;
+	cout <<"=================================" << endl;
+	cout << "Greedy Elevator Total Time:" << greedyLoading(pkgs.data(), packNumber) << endl;
 	
 	return 0;
 }
